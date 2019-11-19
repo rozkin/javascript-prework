e9b7919066fc3671e13adef32fe1136bf2e494f0
Kinga Rozej <kinga.rozej@gmail.com>
2019-11-19 17:50:08 +0100

    Change remote script.js file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
+function getMoveName(argMoveId){
+  if(argMoveId == 1){
+    return 'kamień';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
+  if(argMoveId == 2){
+    return 'papier';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
+  if(argMoveId == 3){
+    return 'nożyce';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
+/* if(randomNumber == 1){
   computerMove = 'kamień';
 } else if (randomNumber == 2) {
   computerMove = 'papier';
@@ -12,23 +33,22 @@ if(randomNumber == 1){
   computerMove = 'nożyce';
 }
 
-printMessage('Mój ruch to: ' + computerMove);
-
+printMessage('Mój ruch to: ' + computerMove); */
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(randomNumber);
 
-if(playerInput == '1'){
+/* if(playerInput == '1'){
   playerMove = 'kamień';
 } else if (playerInput == '2') {
   playerMove = 'papier';
 } else if (playerInput == '3') {
   playerMove = 'nożyce';
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Twój ruch to: ' + playerMove); */
 
 
 if( computerMove == 'kamień' && playerMove == 'papier'){
@@ -50,3 +70,7 @@ if( computerMove == playerMove){
   printMessage('Wybraliście takie same ruchy. Spróbuj ponownie!');
 if( playerMove == 'nieznany ruch'){
   printMessage('Czy na pewno wybrałaś/eś 1, 2 lub 3?');
+
+function displayResult (argComputerMove, argPlayerMove) {
+  printMessage('Zagrałem ' + argComputerMove + ', a ty ' + argPlayerMove);
+}

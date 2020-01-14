e53b6d5b8478e1f9724029ab5d543b586efd46ae
Kinga Rozej <kinga.rozej@gmail.com>
2020-01-14 19:06:12 +0100

    Fixed JS file

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
-function getMoveName(argMoveId){
-  if(argMoveId == 1){
+function getMoveName(argMoveId) {
+  if (argMoveId == 1) {
     return 'kamień';
-  } else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-    return 'nieznany ruch';
-  }
-  if(argMoveId == 2){
+  } else if (argMoveId == 2) {
     return 'papier';
-  } else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-    return 'nieznany ruch';
-  }
-  if(argMoveId == 3){
+  } else if (argMoveId == 3) {
     return 'nożyce';
   } else {
     printMessage('Nie znam ruchu o id ' + argMoveId + '.');
@@ -25,74 +17,31 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = getMoveName(randomNumber);
 
-/* if(randomNumber == 1){
-  computerMove = 'kamień';
-} else if (randomNumber == 2) {
-  computerMove = 'papier';
-} else if (randomNumber == 3) {
-  computerMove = 'nożyce';
-}*/
-
-printMessage('Mój ruch to: ' + computerMove);
+printMessage('Komputer ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = getMoveName(randomNumber);
-
-/* if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if (playerInput == '2') {
-  playerMove = 'papier';
-} else if (playerInput == '3') {
-  playerMove = 'nożyce'; */
+let playerMove = getMoveName(playerInput);
 
 printMessage('Twój ruch to: ' + playerMove);
 
-
-/*if( computerMove == 'kamień' && playerMove == 'papier'){
-  printMessage('Ty wygrywasz!');
-} else {
-  printMessage('Spróbuj jeszcze raz!');
-}
-if( computerMove == 'nożyce' && playerMove == 'kamień'){
-  printMessage('Ty wygrywasz!');
-} else {
-  printMessage('Spróbuj jeszzce raz!');
-}
-if( computerMove == 'papier' && playerMove == 'nożyce'){
-  printMessage('Ty wygrywasz!');
-} else if (computerMove == playerMove){
-  printMessage('Wybraliście takie same ruchy. Spróbuj ponownie!');
-} else if (playerMove == 'nieznany ruch'){
-  printMessage('Czy na pewno wybrałaś/eś 1, 2 lub 3?');
-} else {
-  printMessage('Spróbuj jeszzce raz!');
-}
-if( computerMove == playerMove){
-  printMessage('Wybraliście takie same ruchy. Spróbuj ponownie!');
-if( playerMove == 'nieznany ruch'){
-  printMessage('Czy na pewno wybrałaś/eś 1, 2 lub 3?');
-
-function displayResult (argComputerMove, argPlayerMove) {
-  printMessage('Zagrałem ' + argComputerMove + ', a ty ' + argPlayerMove);
-} */
-
-function displayResult(argComputerMove, argPlayerMove){
+function displayResult(argComputerMove, argPlayerMove) {
   console.log('moves:', argComputerMove, argPlayerMove);
-  printMessage('Zagrałem' + argComputerMove + ', a Ty ' + argPlayerMove);
-if(( argComputerMove == 'kamień' && argPlayerMove == 'papier')||(argComputerMove == 'nożyce' && argPlayerMove == 'kamień')||
-(argComputerMove == 'papier' && argPlayerMove == 'nożyce')){
-  printMessage('Ty wygrywasz!');
-} else if (argComputerMove == argPlayerMove){
-  printMessage('Wybraliście takie same ruchy. Spróbuj ponownie!');
-  console.log('Comp i player wybrali ten sam ruch');
-} else if (argPlayerMove == 'nieznany ruch'){
-  printMessage('Czy na pewno wybrałaś/eś 1, 2 lub 3?');
-  console.log('Gracz wybrał nieznany ruch: '+ argPlayerMove);
-} else {
-  printMessage('Niestety, tym razem przegrałeś/aś. Spróbuj jeszcze raz!');
-  console.log('Gracz przgerał')
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+  if ((argComputerMove == 'kamień' && argPlayerMove == 'papier') || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') ||
+    (argComputerMove == 'papier' && argPlayerMove == 'nożyce')) {
+    printMessage('Ty wygrywasz!');
+  } else if (argComputerMove == argPlayerMove) {
+    printMessage('Wybraliście takie same ruchy. Spróbuj ponownie!');
+    console.log('Comp i player wybrali ten sam ruch');
+  } else if (argPlayerMove == 'nieznany ruch') {
+    printMessage('Czy na pewno wybrałaś/eś 1, 2 lub 3?');
+    console.log('Gracz wybrał nieznany ruch: ' + argPlayerMove);
+  } else {
+    printMessage('Niestety, tym razem przegrałeś/aś. Spróbuj jeszcze raz!');
+    console.log('Gracz przgerał')
+  }
 }
-}
+displayResult(computerMove, playerMove);
